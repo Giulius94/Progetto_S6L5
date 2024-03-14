ee9fdb88ec457cc5a1979e54304237d26b0a2892
Giulius94 <giuliuscb@protonmail.com>
2024-03-14 17:10:55 +0100

    ok

--- Progetto_S6:L5_Laravel/Laravel/resources/views/projects.blade.php
+++ Progetto_S6:L5_Laravel/Laravel/resources/views/projects.blade.php
@@ -10,7 +10,8 @@
         <img src="https://www.celoxis.com/cassets/img/pmc/project-management.png" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">{{$value->title}}</h5>
-            <p class="card-text">Tot.Pagine{{$value->pages}} Anno Pubblicazione: {{$value->year}}</p>
+            <p class="card-text">Tot.Pagine{{$value->pages}}</p>
+            <p class="card-text">Anno Pubblicazione: {{$value->year}}</p>
             <a type="button" class="btn btn-outline-info" href="/books?id={{$value->id}}">Info</a>
             <a type="button" class="btn btn-outline-warning" href="/books/{{$value->id}}/edit">Edit</a>
             <a type="button" class="btn btn-outline-danger" href="/books/{{$value->id}}/destroy">Delete</a>
